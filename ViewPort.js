99f2bed7fcd85a33bd4995c3fd97ca66d893a6fc
Glindtvad@gmail.com <44803430+Glindtvad@users.noreply.github.com>
2024-07-26 11:10:34 -0300

    update

--- ViewPort.js
+++ ViewPort.js
@@ -1,8 +1,15 @@
 
-
-function getViewportUnits(){
-    const placeholder = document.getElementById("placeholder");
-    const vh = placeholder.getBoundingClientRect().height;
-    const vw = placeholder.getBoundingClientRect().width;
-    console.log({vh: vh, vw: vw});
+function viewport() {
+  var e = window, a = 'inner';
+  if (!('innerWidth' in window )) {
+      a = 'client';
+      e = document.documentElement || document.body;
   }
+  return { width : e[ a+'Width' ] , height : e[ a+'Height' ] };
+}
+jQuery(window).resize(function(){
+  var vw = (viewport().width/100);
+  jQuery('html').css({
+      'font-size' : vw + 'px'
+  });
+});
